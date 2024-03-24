2d224574fa0ded11899a37fae07848f14136ce58
TTdebian <tejatilak26@gmail.com>
2024-03-24 10:35:52 +0530

    added two more words to the search

--- auto_searcher.py
+++ auto_searcher.py
@@ -8,9 +8,11 @@ nsearch=int(input("\nEnter number of search==>"))
 i=0
 print("\nHang On!!")
 while(i<nsearch):
-    word=r.word()
+    word1=r.word()
+    word2=r.word()
+    word3=r.word()
     randomint=random.randint(3,8)
     time.sleep(randomint)
-    webbrowser.open("https://www.bing.com/search?q="+word+"&qs=n&form=QBRE&sp=-1&lq=0&pq=m&sc=11-1&sk=&cvid=BD43CEF94CB54B6CBC728896AF0A144A&ghsh=0&ghacc=0&ghpl=")
+    webbrowser.open("https://www.bing.com/search?q="+word1+"+"+word2+"+"+word3+"&qs=n&form=QBRE&sp=-1&lq=0&pq=m&sc=11-1&sk=&cvid=BD43CEF94CB54B6CBC728896AF0A144A&ghsh=0&ghacc=0&ghpl=")
     i=i+1 
 print("\n**ENDED**")
